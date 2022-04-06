ae9ee6c8ebc3b82f5189ceac09bfbcd6bc34e530
slipmip <slipmip@outlook.dk>
2022-04-06 21:44:20 +0200

    added hello response

--- slipbot.js
+++ slipbot.js
@@ -1,1 +1,21 @@
-console.log("death")
+console.log('Initializing SLiPBoT')
+
+const Discord = require('discord.js')
+const client = new Discord.Client()
+client.login('')
+
+client.on('ready', readyDiscord)
+
+function readyDiscord() {
+    console.log('initialized')
+}
+
+client.on('message', gotMessage)
+
+function gotMessage(msg) {
+    console.log(msg.content)
+    if (msg.channel.id == '659842093379878924' && msg.content == 'Hello slip') {
+        // msg.reply('Hello') //also tags them in the message
+        msg.channel.send('Hello')
+    }
+}
