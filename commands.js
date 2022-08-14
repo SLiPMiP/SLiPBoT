06d2911a61607602aef0e52f25792d90d2b37ab3
slipmip <slipmip@outlook.dk>
2022-08-15 00:24:54 +0200

    added compatebility for commands without prefix

--- commands.js
+++ commands.js
@@ -1,25 +1,32 @@
 // const who = require("./commands/who.js")
 // const asked = require('./commands/asked.js')
 const gif = require('./commands/gif.js')
-// const mastermind = require('./commands/mastermind.js')
+    // const mastermind = require('./commands/mastermind.js')
 
-const commands = {gif }
+const commands = { gif }
 
-module.exports = function (msg) {
+module.exports = function(msg) {
     console.log(msg.author.username, ':', msg.content)
 
     let tokens = msg.content.split(" ")
     let command = tokens.shift()
+
+
+    if (msg.author.id == process.env.BOTID) {
+        return
+    } else {
+        console.log("   cmd:", command)
+    }
+
+
     if (command.charAt(0) === "!") {
         command = command.substring(1)
         commands[command](msg, tokens)
     }
-    
-    if (msg.author.id == process.env.BOTID) {
-        return
-    }
-    else {
-        console.log("   cmd:", command)
+
+
+    if (commands.includes(command)) {
+        commands[command](msg, tokens)
     }
 
 }
