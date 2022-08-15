15eda73d8d50f0ca1da77c9360c20290fde73603
slipmip <slipmip@outlook.dk>
2022-08-15 10:34:27 +0200

    remembered how tokens became args and how to do linebreaks

--- commands/mastermind.js
+++ commands/mastermind.js
@@ -1,8 +1,13 @@
 let guess = (':white_square_button:')
-let indic, indicator = (':radio_button:')
+let indicator = (':radio_button:')
 
 module.exports = function(msg, args) {
-    msg.channel.send('Mastermind!')
-    msg.channel.send(guess)
-    msg.channel.send(indic)
+    if (args[0] == 'test') {
+        msg.channel.send('Mastermind!')
+        msg.channel.send(guess)
+        msg.channel.send(indicator)
+    }
+    if (args.length < 1) {
+        msg.channel.send("You must define what you want\nheres a list of all the commands\nmastermind + \nSTART;END;RESTART\n;GUESS(or just g)\n;HELP (to see this command)\nTo start a game, use the command:\n!mastermind start")
+    }
 }
